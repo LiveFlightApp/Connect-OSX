b9f56f83595455a8508836c525f33a20d7addec8
Cameron Carmichael Alonso <cameron@carmichaelalonso.co.uk>
2015-12-25 23:41:29 +0100

    Cleaned up files

---
+++ LiveFlight/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  LiveFlight
+//
+//  Created by Cameron Carmichael Alonso on 25/12/2015.
+//  Copyright © 2015 Cameron Carmichael Alonso. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: NSViewController {
+
+}
